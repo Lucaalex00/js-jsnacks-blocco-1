bee691e6c604aa337a68f606b4ca3f5da955e510
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-23 12:20:24 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,20 @@
+//L'utente inserisce due numeri in successione, con due prompt.
+//il software stampa il maggiore.
+
+let numberOne = prompt('Inserisci un numero')
+Number(numberOne)
+let numberTwo = prompt('Inserisci un altro numero')
+Number(numberTwo)
+
+
+if (numberOne > numberTwo) {
+    document.writeln(`Il Primo numero è maggiore (${numberOne})`);
+    console.log(numberOne)
+} else if (numberOne > numberTwo) {
+    document.writeln(`Il Secondo numero è maggiore (${numberTwo})`);
+    console.log(numberTwo)
+} else {
+    console.log('PARI')
+    document.writeln(`Il Primo numero (${numberOne}) è uguale al secondo numero (${numberTwo})`);
+}
+
