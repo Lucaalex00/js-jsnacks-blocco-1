c8fdc181751c6c6e4520c4ef2a5f5d1dc8dad5a3
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-26 10:50:34 +0100

    Add do/while cycle

--- snack-3.1/script.js
+++ snack-3.1/script.js
@@ -1,18 +1,27 @@
 // il software deve chiedere per 10 volte all'utente di inserire un numero.
 // il programma stampa la somma di tutti i numeri inseriti
 let i = 0;
+
+//WHILE
+
 /* while (i < 10) {
 
     const userNumber = prompt('Inserisci il numero')
     console.log(`il numero scelto è  ${userNumber}`)
-
     i++
-
 }
  */
+//FOR
 
-for (let il = 0; il < 10; il++) {
+/* for (let i = 0; i < 10; i++) {
     const userNumber = prompt('inserisci il numero')
     console.log(`il numero scelto è  ${userNumber}`)
 
-}
+} */
+
+
+do {
+    const userNumber = prompt('inserisci il numero')
+    console.log(`il numero scelto è  ${userNumber}`)
+    i++
+} while (i < 10)
