bc94ebf7bc7b087eeeb70ede7de91369686c3d54
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-26 10:47:20 +0100

    add Snack-3.1 commit

---
+++ snack-3.1/script.js
@@ -0,0 +1,18 @@
+// il software deve chiedere per 10 volte all'utente di inserire un numero.
+// il programma stampa la somma di tutti i numeri inseriti
+let i = 0;
+/* while (i < 10) {
+
+    const userNumber = prompt('Inserisci il numero')
+    console.log(`il numero scelto è  ${userNumber}`)
+
+    i++
+
+}
+ */
+
+for (let il = 0; il < 10; il++) {
+    const userNumber = prompt('inserisci il numero')
+    console.log(`il numero scelto è  ${userNumber}`)
+
+}
